17507616da1b0a7831f28ed243703164e496f4b1
hoangson58 <nexttopmu123@gmail.com>
2018-01-22 14:49:42 +0700

    'Ho'

---
+++ js.main.js
@@ -0,0 +1,73 @@
+﻿function htt_orion(){
+
+setTimeout(function(){
+
+
+swal({  
+
+ title: "Bạn nhận được một món quà !",   
+text: "HS vừa gửi bạn một món quà đặc biệt nhân ngày sinh nhật của bạn ", 
+ imageUrl: "https://scontent.fhan2-3.fna.fbcdn.net/v/t1.0-9/25498095_1574130009336783_4849188595150105319_n.jpg?oh=5af3daef428a29f05260144c5b72e85e&oe=5AB95C64",
+  showCancelButton: true,
+   confirmButtonColor: "#DD6B55", 
+  confirmButtonText: "Nhận quà ngay",
+   cancelButtonText: "Không cần, cám ơn .", 
+  closeOnConfirm: false,  
+ closeOnCancel: false }, 
+function(isConfirm){  
+ if (isConfirm) {   
+ 
+
+swal({  
+ title: "Bạn có phải là Nguyệt ? ", 
+  text: "Nhập mã xác nhận ",  
+ type: "input",  
+ showCancelButton: true,  
+ closeOnConfirm: false,   
+ showLoaderOnConfirm: true,
+animation: "slide-from-top",  
+ inputPlaceholder: "Mã xác nhận là gì ?" },
+ function(inputValue){   
+if (inputValue === false) return false;   
+   if (inputValue !== "aibiet") {   
+  swal.showInputError("Nhập sai rồi kìa :v ");   
+  return false   }   
+ 
+setTimeout(function(){
+
+
+swal({  
+ title: "Nhập tên món quà nào đó ", 
+  text: "Nguyệt muốn nhận quà gì nào ^^ ? ",  
+ type: "input",  
+ showCancelButton: true,  
+ closeOnConfirm: false,   
+animation: "slide-from-top",  
+
+ inputPlaceholder: "Nhập vào là có luôn ^^ " },
+ function(inputValue){   
+if (inputValue === false) return false;   
+   if (inputValue === "") {   
+  swal.showInputError("Không thể để trống được ");   
+  return false   }  
+ swal("OK", "Món quà " + inputValue + " sẽ được tặng cho bạn vào... năm sau :v   ", "success"); });
+
+
+},3000);
+
+
+
+});
+
+ }
+else {   
+  swal("KHÔNG NHẬN ", "Bạn vừa từ chối món quà từ HS == ,    Ấn F5 nếu muốn nhận lại quà :D ", "error"); 
+
+  } });
+
+
+
+},8000);
+}
+htt_orion();
+
